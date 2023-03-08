323744383de3872a836e63f2b80b1e8fb9e7be32
andyrs <andy337@hotmail.es>
2023-03-08 17:03:37 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-'use strict'
-
-window.addEventListener('load', function () {
-
-
+window.addEventListener('load', () => {
     /***************************toogle  icon menu bar******************************************/
     let menuIcon = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
-
 
     menuIcon.onclick = () => {
         menuIcon.classList.toggle('bx-x');
         navbar.classList.toggle('active');
     };
+
 
     /*************Scroll sections active link***********************/
     let sections = document.querySelectorAll('section');
@@ -31,15 +27,50 @@ window.addEventListener('load', function () {
                 });
             }
         });
-    };
 
-    /***************************Sticky navbar******************************************/
-    let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+        /***************************Sticky navbar******************************************/
+        let header = document.querySelector('header');
 
-    /***************************remove toglle menu icon and navbar when click navbar link (Scroll) / Quitar menu cuando de un click******************************************/
+        header.classList.toggle('sticky', window.scrollY > 100);
+
+        /***************remove toglle menu icon and navbar when click navbar link (Scroll) / Quitar menu cuando de un click**************/
         menuIcon.classList.remove('bx-x');
         navbar.classList.remove('active');
 
-});
+    };
+
+
+    /***************************Scroll reveal******************************************/
+    ScrollReveal({
+        /* reset: true,*/
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', {
+        origin: 'top'
+    });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {
+        origin: 'bottom'
+    });
+
+    ScrollReveal().reveal('.home-content h1, .about-img ', {
+        origin: 'left'
+    });
+
+    ScrollReveal().reveal('.home-content p, .about-content ', {
+        origin: 'right'
+    });
+
+
+    /*TypedJS*/
+    const typed = new Typed('.multiple-text', {
+        strings: ['Fronted Developer', 'SEO', 'Blogger'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+})
