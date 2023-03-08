0a126195e9140054fb922c439da640dcb5a9401b
andyrs <andy337@hotmail.es>
2023-03-08 13:28:34 -0400

    update-new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-'use strict'
+window.addEventListener('load', function () {
 
-window.addEventListener('load',function(){
-    
-})
+
+    /***************************toogle  icon menu bar******************************************/
+    let menuIocn = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+
+    menuIocn.onclick = () => {
+        menuIocn.classList.toggle('bx-x')
+        navbar.classList.toggle('active')
+    }
+
+    /*************Scroll sections active link***********************/
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            }
+        });
+    };
+
+    /***************************Sticky navbar******************************************/
+
+    let header = document.querySelector('header');
+
+
+    header.classList.toogle('sticky', window.scrollY > 100);
+
+    /***************************remove toglle menu icon and navbar when click navbar link (Scroll) / Quitar menu cuando de un click******************************************/
+    menuIocn.classList.remove('bx-x')
+    navbar.classList.remove('active')
+
+
+    /***************************Tipado jS/ Typed JS******************************************/
+   
+
+});
+
---
+++ js/tiped.js
@@ -0,0 +1,12 @@
+'use strict'
+
+window.addEventListener('load', function () {
+
+    const typed = new Typed('.multiple-text', {
+        strings: ['Fronted Developer', 'SEO', 'Blogger'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+})
